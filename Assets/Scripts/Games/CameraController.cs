cfeaca91c4d4c23fbc2f7087226bbe0dc9e35bc1
Pedro Nunes <pnunes91@gmail.com>
2020-05-27 15:50:03 -0300

    Fixed Camera Controls for Game Tree

--- Assets/Scripts/Games/CameraController.cs
+++ Assets/Scripts/Games/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditorInternal;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
@@ -37,22 +38,37 @@ public class CameraController : MonoBehaviour {
         
     private void SetCamera (int depth, int branching, float stepY) {
         float totalLeafs = Mathf.Pow (branching, depth);
+        float camSizeX = (totalLeafs + 1);
 
-        float rootSize = 1 + ((branching * (depth)) / (2 * cam.aspect));
-
-        float sizeY = ((stepY * depth) / 2) + rootSize;
-
-        float sizeX = ((totalLeafs + 1) / cam.aspect) / 2;
-
-        cam.orthographicSize = Mathf.Max (sizeX, sizeY) + border;
+        float treeSizeY = 0;
+        for (int n = 0; n < depth; n++) {
+            treeSizeY += 1 + ((branching * (depth - n)) * (1 / Camera.main.aspect));
+        }
+        float camSizeY = (treeSizeY);
+        
         
         float posY = TreeNode.Leaves[0].GO.transform.position.y / 2;
         cam.transform.position = new Vector3 (0f, posY, cam.transform.position.z);
 
-        area = new Bounds (cam.transform.position, new Vector3 (2 * cam.orthographicSize * cam.aspect, 2 * cam.orthographicSize));
+        
+        //area = new Bounds (cam.transform.position, new Vector3 (2 * cam.orthographicSize * cam.aspect, 2 * cam.orthographicSize));
+        Vector3 areaSize = new Vector3(camSizeX, camSizeY, 0f );
+        
+        area = new Bounds (cam.transform.position, areaSize);
+
+        float areaAspect = area.size.x / area.size.y;
+
+        if (areaAspect > cam.aspect) {
+            cam.orthographicSize = (areaSize.x / cam.aspect)/ 2;
+        }
+        else {
+            cam.orthographicSize = (areaSize.y) / 2;
+        }
+            //cam.orthographicSize = Mathf.Max( areaSize.x * cam.aspect / 2, areaSize.y / 2 ) + border;
+            
 
         //setup the collider size for checking mouse interactions properly
-        BoxCollider2D clickArea= this.GetComponent<BoxCollider2D>();
+        BoxCollider2D clickArea = this.GetComponent<BoxCollider2D>();
         if (clickArea != null) {
             clickArea.size = new Vector2(2 * cam.orthographicSize * cam.aspect, 2 * cam.orthographicSize);
             clickArea.offset = new Vector2 (0, posY);
@@ -82,10 +98,12 @@ public class CameraController : MonoBehaviour {
             Vector3 origMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
             //Zoom
-            if (Input.GetAxis("Mouse ScrollWheel") < 0) //zoom out
-                cam.orthographicSize = Mathf.Min(cam.orthographicSize + cam.orthographicSize * wheel, area.extents.y);
-            else if (Input.GetAxis("Mouse ScrollWheel") > 0) //zoom in
-                cam.orthographicSize = Mathf.Max(Camera.main.orthographicSize - cam.orthographicSize * wheel, minSize);
+            if (Input.GetAxis( "Mouse ScrollWheel" ) < 0) { //zoom out
+                float maxCamSize = Mathf.Max( area.extents.y, area.extents.x / cam.aspect );
+                cam.orthographicSize = Mathf.Min( cam.orthographicSize + cam.orthographicSize * wheel, maxCamSize);
+            }
+            else if (Input.GetAxis( "Mouse ScrollWheel" ) > 0) //zoom in
+                cam.orthographicSize = Mathf.Max( Camera.main.orthographicSize - cam.orthographicSize * wheel, minSize );
 
             //Get the new Viewport coordinates for the new position of the object.
             Vector3 finalMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -132,8 +150,20 @@ public class CameraController : MonoBehaviour {
     private Vector3 ClampMovement (Vector3 movePos) {
         Vector3 pos = new Vector3 ();
         //pos.x = Mathf.Clamp (movePos.x, area.min.x + cam.orthographicSize / 3 * 4, area.max.x - cam.orthographicSize / 3 * 4);
-        pos.x = Mathf.Clamp (movePos.x, area.min.x + cam.orthographicSize * cam.aspect, area.max.x - cam.orthographicSize * cam.aspect);
-        pos.y = Mathf.Clamp (movePos.y, area.min.y + cam.orthographicSize, area.max.y - cam.orthographicSize);
+        //se a camera for maior que a area, nao deve se mover em y
+        if (cam.orthographicSize < area.extents.y) {
+            pos.y = Mathf.Clamp (movePos.y, area.min.y + cam.orthographicSize, area.max.y - cam.orthographicSize);
+        }
+        else {
+            pos.y = area.center.y;
+        }
+
+        if (cam.orthographicSize * cam.aspect < area.extents.x) {
+            pos.x = Mathf.Clamp (movePos.x, area.min.x + cam.orthographicSize * cam.aspect, area.max.x - cam.orthographicSize * cam.aspect);
+        }
+        else {
+            pos.x = area.center.x;
+        }
         pos.z = movePos.z;
         return pos;
     }
